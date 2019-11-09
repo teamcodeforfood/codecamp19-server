714e412fb72e3dfe96e5204f9909441a30a2d0fe
Josh Beitler <joshbeitler@gmail.com>
2019-11-08 23:01:15 -0700

    Update routes.js

--- routes/events/routes.js
+++ routes/events/routes.js
@@ -4,20 +4,20 @@ let mw = require('../middleware.js');
 let handler = require('./handlers.js');
 
 let roles = require('./roles/routes.js');
-router.use('/:id/roles', roles);
+router.use('/:event_id/roles', roles);
 let divisions = require('./divisions/routes.js');
-router.use('/:id/divisions', divisions);
+router.use('/:event_id/divisions', divisions);
 let categories = require('./categories/routes.js');
-router.use('/:id/categories', categories);
+router.use('/:event_id/categories', categories);
 let teams = require('./teams/routes.js');
-router.use("/:id/teams", teams);
+router.use("/:event_id/teams", teams);
 let judges = require('./judges/routes.js');
-router.get('/:id/judges', judges);
+router.get('/:event_id/judges', judges);
 router.get('/', handler.listEvents);
 router.post('/', mw.verifyToken, handler.createEvent);
-router.get('/:id', handler.getEvent);
-router.patch('/:id', mw.verifyToken, handler.updateEvent);
-router.delete('/:id', mw.verifyToken, handler.deleteEvent);
+router.get('/:event_id', handler.getEvent);
+router.patch('/:event_id', mw.verifyToken, handler.updateEvent);
+router.delete('/:event_id', mw.verifyToken, handler.deleteEvent);
 
 router.get('/:id/participants', handler.listParticipants);
 
